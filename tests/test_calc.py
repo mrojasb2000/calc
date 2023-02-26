89efbfc9fe054d1cc83356930c04712bb01a793e
Mauricio Rojas <mrojasb2000@gmail.com>
2023-02-26 18:44:17 -0300

    refactor: added two or three parameters

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -7,3 +7,11 @@ def test_add_two_numbers():
     actual = c.add(4, 5)
 
     assert actual == expected
+
+def test_add_three_numbers():
+    c = Calc()
+
+    expected = 15
+    actual = c.add(4, 5, 6)
+
+    assert actual == expected    
