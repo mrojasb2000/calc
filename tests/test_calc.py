35a6a42c912971e9f0d1d0841002584ad47e5e64
Mauricio Rojas <mrojasb2000@gmail.com>
2023-02-26 21:55:05 -0300

    feat: added multication two numbers

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -14,3 +14,10 @@ def test_substract_two_numbers():
     actual = c.sub(10, 3)
 
     assert actual == expected
+
+def test_mul_two_numbers():
+    c = Calc()
+    expected = 24
+    actual = c.mul(6, 4)
+
+    assert actual == expected
